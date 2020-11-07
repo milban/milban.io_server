7ae164aeeb72713b09c757c61e35b65afb78804a
JunHyeock Park <wnsgur6311@gmail.com>
2020-11-07 23:39:17 +0900

    Set graphql config
    schema first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    /*
+      forRoot option
+      https://www.apollographql.com/docs/apollo-server/api/apollo-server/#constructor-options-lt-ApolloServer-gt
+     */
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: path.join(process.cwd(), 'src/graphql.schema.ts'),
+        outputAs: 'class',
+        emitTypenameField: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
