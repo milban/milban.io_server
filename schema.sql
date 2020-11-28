0c5c78158042a48a5e881385bd4f10ee7e2e772a
JunHyeock Park <wnsgur6311@gmail.com>
2020-11-28 12:15:03 +0900

    Init prisma

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA milbanio;
+
+CREATE TABLE "milbanio"."User" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+  "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
+  "userId" VARCHAR(255) UNIQUE NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
