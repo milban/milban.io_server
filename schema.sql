dedb83b385f8555fee4c267b5b6a23b88057eae7
JunHyeock Park <wnsgur6311@gmail.com>
2020-11-28 20:04:11 +0900

    DB id row type을 uuid로 변경

--- schema.sql
+++ schema.sql
@@ -1,7 +1,11 @@
 CREATE SCHEMA milbanio;
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
 CREATE TABLE "milbanio"."User" (
-  id SERIAL PRIMARY KEY NOT NULL,
+  id uuid DEFAULT uuid_generate_v4 (),
+  PRIMARY KEY (id),
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
   "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
   "userId" VARCHAR(255) UNIQUE NOT NULL,
